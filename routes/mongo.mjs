8557bf36eb88ca11116a66d02f317f450a5fea65
OlganeOlga <o_yegorova@yahoo.com>
2024-09-16 11:41:47 +0200

    mongoJSONroutes fungerar

--- routes/mongo.mjs
+++ routes/mongo.mjs
@@ -54,9 +54,8 @@ router.get("/mongo/deleatByTitle/:title", async(req, res) => {
     const client = new MongoClient(uri);
     await client.connect();
     const db = client.db(dbName);
-    console.log(req.params.title)
     try {
-        const document = await db.collection(colName).deleteOne({'data.title': req.params.title});
+        const document = await db.collection(colName).deleteOne({title: req.params.title});
         res.json({document: document});
     } catch (error) {
         console.log('error in deleating document by title: ', error);
@@ -68,31 +67,31 @@ router.get("/mongo/deleatByTitle/:title", async(req, res) => {
 
 
 
-// add new document
-router.get("/add/:title/:content", async(req, res) => {
-     // Prevent conflict with the 'deleat' route
-     if (req.params.title === "deleat") {
-        return res.status(400).json({ error: "Invalid route usage." });
-    }
-    const data = {
-        data: {
-            title: req.params.title,
-            content: req.params.content
-        }
-    };
-    const client = new MongoClient(uri);
-    await client.connect();
-    const db = client.db(dbName);
-    try {
-        const resut = await db.collection(colName).insertOne(data);
-        return res.redirect('/mongo/');
-    } catch (error) {
-        console.log('error in inserting: ', error);
-        res.json({ error: error});
-    } finally {
-        await client.close()
-    }
-});
+// // add new document or update existing
+// router.get("/add/:title/:content", async(req, res) => {
+//      // Prevent conflict with the 'deleat' route
+//      if (req.params.title === "deleat") {
+//         return res.status(400).json({ error: "Invalid route usage." });
+//     }
+//     const data = {
+//         data: {
+//             title: req.params.title,
+//             content: req.params.content
+//         }
+//     };
+//     const client = new MongoClient(uri);
+//     await client.connect();
+//     const db = client.db(dbName);
+//     try {
+//         const resut = await db.collection(colName).insertOne(data);
+//         return res.redirect('/mongo/');
+//     } catch (error) {
+//         console.log('error in inserting: ', error);
+//         res.json({ error: error});
+//     } finally {
+//         await client.close()
+//     }
+// });
 
 // get one dcument as JSON
 router.get("/:id", async(req, res) => {
@@ -114,10 +113,10 @@ router.get("/:id", async(req, res) => {
 
 router.get('/mongo/getByTitle/:title', async (req, res) => {
     const searched = req.params.title;
-    const query = {'data.title': searched};
+    const query = {title: searched};
 
     const options = {
-        projection: { _id: 1, 'data.title': 0, 'data.content': 0 }
+        projection: { _id: 1, title: 0, content: 0 }
     }
     const client = new MongoClient(uri);
     await client.connect();
@@ -135,10 +134,10 @@ router.get('/mongo/getByTitle/:title', async (req, res) => {
 
 
 // uppdate or add a new document with title and content
-router.get('/add/:title/:content', async (req, res) => {
+router.get('/mongo/add/:title/:content', async (req, res) => {
     const data = req.params;
  
-    const filter = { 'data.title': data.title };
+    const filter = { title: data.title };
 
     const options = { upsert: true }; // add document if the docuent with this title is note found
     const updateDoc = {
@@ -163,23 +162,26 @@ router.get('/add/:title/:content', async (req, res) => {
 });
 
 // add an new unnamed document
-router.get('/new', async (req, res) => {
+router.get('/mongo/new', async (req, res) => {
     const client = new MongoClient(uri);
     await client.connect();
     const db = client.db(dbName);
+    const query = {title: /^unnamed\d+$/};
+    const options = {
+        projection: { _id: 1, title: 0, content: 0 }
+    }
     try {
         //find all document with name "unnamed" and digits afterwords:
-        const unnamedDocuments = await db.collection(colName).find({name: /^unnamed\d+$/}).toArray();
-        const number = unnamedDocuments.length;
+        // const unnamedDocuments = await db.collection(colName).find(query, options).toArray();
+        // const number = unnamedDocuments.length + 1;
 
         //give number for the unnamed document
         const data = {
-            data: {
-                title: "unnamed" + number,
+                //title: "unnamed" + number,
+                title: "unnamed",
                 content: ""
-            }
         };
-        const document = await db.collection(colName).addOne(data);
+        const document = await db.collection(colName).insertOne(data);
         res.json({ document: document });
     } catch (error) {
         console.log('error in adding empty document: ', error);
